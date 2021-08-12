5600f5265d61fbad3852d3c62a9f9ff788e004f7
Taurus0205 <dolchin1990@mail.ru>
2021-08-12 06:12:56 +0500

    Add initial changes with online array

--- js/main.js
+++ js/main.js
@@ -1,11 +1,10 @@
 const elMovieList = selectElement(".movies__list");
 const elMovieTemplate = selectElement(".movie__template").content;
 
-// modal
+// // modal
 const elModalInfo = selectElement(".modal");
 const elModalImg = selectElement(".modal__img");
 const elModalHeading = selectElement(".modal__heading");
-const elModalDescription = selectElement(".modal__paragraph-description");
 const elModalTime = selectElement(".modal__time");
 const elModalList = selectElement(".modal__genres-list");
 const elModalCloseBtn = selectElement(".modal__close-btn");
@@ -20,38 +19,57 @@ elModalCloseBtn.addEventListener("click", (evt) => {
   elModalInfo.classList.remove("modal--active");
 });
 
-// Form elements
+// // Form elements
 const elForm = selectElement(".movies__form");
 const elMoviesInput = selectElement(".movies__form__input", elForm);
 const elSelectGenres = selectElement(".movies__form__select", elForm);
 const elSelectSort = selectElement(".sort-films", elForm);
+
+// form submit
+elForm.addEventListener("submit", (evt) => {
+  evt.preventDefault();
+
+  async function fetchFilms() {
+    const APIKey = "2b278381";
+    const APISearch = elMoviesInput.value;
+    const response = await fetch(
+      "http://www.omdbapi.com/?apikey=" + APIKey + "&s=" + APISearch
+    );
+
+    const data = await response.json();
+    const filmArr = data.Search;
+
+    const moviesInput = elMoviesInput.value.trim();
+    const regex = new RegExp(moviesInput, "gi");
+    const searchedFilms = filmArr.filter((film) => film.Title.match(regex));
+
+    filmRender(searchedFilms, elMovieList);
+  }
+
+  fetchFilms();
+});
 
 // Rendering genres
 
 function renderGenres(filmArr, element) {
   const result = [];
   filmArr.forEach((film) => {
-    film.genres.forEach((genre) => {
-      if (!result.includes(genre)) {
-        result.push(genre);
-      }
-    });
+    if (!result.includes(film.Type)) {
+      result.push(film.Type);
+    }
   });
 
-  result.forEach((genre) => {
+  // element.innerHTML = null;
+  result.forEach((type) => {
     const newOption = createDOM("option");
-    newOption.value = genre;
-    newOption.textContent = genre;
+    newOption.value = type;
+    newOption.textContent = type;
 
     element.appendChild(newOption);
   });
 }
 
-renderGenres(films, elSelectGenres);
-
-// rendering by genres
-
-// Rendering films
+// // Rendering films
 function filmRender(filmArr, element) {
   element.innerHTML = null;
   filmArr.forEach((film) => {
@@ -59,112 +77,103 @@ function filmRender(filmArr, element) {
 
     selectElement(".movies__img", movieTemplate).setAttribute(
       "src",
-      film.poster
+      film.Poster
     );
     selectElement(".movies__img", movieTemplate).setAttribute(
       "alt",
-      film.title
+      film.Title
     );
 
-    selectElement(".movies__heading", movieTemplate).textContent = film.title;
+    selectElement(".movies__heading", movieTemplate).textContent = film.Title;
 
     const elMoreBtn = selectElement(".movies__more-btn", movieTemplate);
-    elMoreBtn.dataset.film_id = film.id;
+    elMoreBtn.dataset.film_id = film.imdbID;
     elMoreBtn.addEventListener("click", (evt) => {
       elModalInfo.classList.add("modal--active");
       const filmId = evt.target.dataset.film_id;
-      const foundFilms = filmArr.find((item) => item.id === filmId);
+      const foundFilms = filmArr.find((item) => item.imdbID === filmId);
 
-      elModalImg.setAttribute("src", foundFilms.poster);
-      elModalHeading.textContent = foundFilms.title;
-      elModalDescription.textContent = foundFilms.overview;
-      elModalTime.textContent = normalizeDate(foundFilms.release_date);
+      elModalImg.setAttribute("src", foundFilms.Poster);
+      elModalHeading.textContent = foundFilms.Title;
+      elModalTime.textContent = "Year: " + foundFilms.Year;
 
       elModalList.innerHTML = null;
-      foundFilms.genres.forEach((genre) => {
-        const newLi = createDOM("li");
-        newLi.textContent = genre;
-        elModalList.appendChild(newLi);
-      });
+      const newLi = createDOM("li");
+      newLi.classList.add("modal__genres-item");
+      newLi.textContent = "Type: " + foundFilms.Type;
+      elModalList.appendChild(newLi);
     });
 
     element.appendChild(movieTemplate);
   });
 }
 
-filmRender(films, elMovieList);
+// renderGenres(searchedFilms, elSelectGenres);
 
-function sortFilms(filmArr, format) {
-  // const sortedAlph = filmArr.sort((a, b) => {
-  //   if (a.title > b.title) {
-  //     return 1;
-  //   } else if (a.title < b.title) {
-  //     return -1;
-  //   } else {
-  //     return 0;
-  //   }
-  // });
+// function sortFilms(filmArr, format) {
+//   // const sortedAlph = filmArr.sort((a, b) => {
+//   //   if (a.title > b.title) {
+//   //     return 1;
+//   //   } else if (a.title < b.title) {
+//   //     return -1;
+//   //   } else {
+//   //     return 0;
+//   //   }
+//   // });
 
-  const sortedDate = filmArr.sort((a, b) => a.release_date - b.release_date);
+//   const sortedDate = filmArr.sort((a, b) => a.Year - b.Year);
 
-  //   if (format === "a_z") {
-  //     return sortedAlph;
-  //   } else if (format === "z_a") {
-  //     return sortedAlph.reverse();
-  //   } else if (format === "old_new") {
-  //     return sortedDate;
-  //   } else if (format === "new_old") {
-  //     return sortedDate.reverse();
-  //   }
-  // }
+//   //   if (format === "a_z") {
+//   //     return sortedAlph;
+//   //   } else if (format === "z_a") {
+//   //     return sortedAlph.reverse();
+//   //   } else if (format === "old_new") {
+//   //     return sortedDate;
+//   //   } else if (format === "new_old") {
+//   //     return sortedDate.reverse();
+//   //   }
+//   // }
 
-  if (format === "a_z") {
-    return filmArr.sort((a, b) => {
-      if (a.title > b.title) {
-        return 1;
-      } else if (a.title < b.title) {
-        return -1;
-      } else {
-        return 0;
-      }
-    });
-  } else if (format === "z_a") {
-    return filmArr.sort((a, b) => {
-      if (a.title > b.title) {
-        return -1;
-      } else if (a.title < b.title) {
-        return 1;
-      } else {
-        return 0;
-      }
-    });
-  } else if (format === "old_new") {
-    return sortedDate;
-  } else if (format === "new_old") {
-    return sortedDate.reverse();
-  }
-}
+//   if (format === "a_z") {
+//     return filmArr.sort((a, b) => {
+//       if (a.Title > b.Title) {
+//         return 1;
+//       } else if (a.Title < b.Title) {
+//         return -1;
+//       } else {
+//         return 0;
+//       }
+//     });
+//   } else if (format === "z_a") {
+//     return filmArr.sort((a, b) => {
+//       if (a.Title > b.Title) {
+//         return -1;
+//       } else if (a.Title < b.Title) {
+//         return 1;
+//       } else {
+//         return 0;
+//       }
+//     });
+//   } else if (format === "old_new") {
+//     return sortedDate;
+//   } else if (format === "new_old") {
+//     return sortedDate.reverse();
+//   }
+// }
 
-elForm.addEventListener("submit", (evt) => {
-  evt.preventDefault();
+// const selectGenre = elSelectGenres.value.trim();
+//     const selectSort = elSelectSort.value.trim();
 
-  const moviesInput = elMoviesInput.value.trim();
-  const regex = new RegExp(moviesInput, "gi");
-  const searchedFilms = films.filter((film) => film.title.match(regex));
-  const selectGenre = elSelectGenres.value.trim();
-  const selectSort = elSelectSort.value.trim();
+//     let genredFilms = [];
 
-  let genredFilms = [];
+//     if (selectGenre === "All") {
+//       genredFilms = searchedFilms;
+//     } else {
+//       genredFilms = searchedFilms.filter((film) =>
+//         film.Type.includes(selectGenre)
+//       );
+//     }
 
-  if (selectGenre === "All") {
-    genredFilms = searchedFilms;
-  } else {
-    genredFilms = searchedFilms.filter((film) =>
-      film.genres.includes(selectGenre)
-    );
-  }
+// const sortedFilms = sortFilms(searchedFilms, selectSort);
 
-  const sortedFilms = sortFilms(genredFilms, selectSort);
-
-  filmRender(sortedFilms, elMovieList);
-});
+// filmRender(filmArr, elMovieList);
