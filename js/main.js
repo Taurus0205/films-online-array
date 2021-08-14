1f6658da5051b90bdc292c930cbc56a5d877aeab
Taurus0205 <dolchin1990@mail.ru>
2021-08-14 10:12:00 +0500

    Add pagination

--- js/main.js
+++ js/main.js
@@ -28,40 +28,78 @@ const elSelectGenres = selectElement(".movies__form__select", elForm);
 const elNextBtn = selectElement(".next-btn", elForm);
 const elPreBtn = selectElement(".pre-btn", elForm);
 
+const APIKey = "2b278381";
+let APISearch = "loki";
+let APIPage = 1;
+let APIType = "movies";
+
+// async function
+async function fetchFilms() {
+  elMovieList.innerHTML = "<img src='./images/spinner.svg' alt='Spinner' />";
+  const response = await fetch(
+    "http://www.omdbapi.com/?apikey=" +
+      APIKey +
+      "&s=" +
+      APISearch +
+      "&type" +
+      APIType +
+      "&page=" +
+      APIPage
+  );
+
+  const data = await response.json();
+  const filmArr = data.Search;
+
+  if (filmArr.length) {
+    filmRender(filmArr, elMovieList);
+  }
+
+  if (APIPage <= 1) {
+    elPreBtn.disabled = true;
+    // elPreBtn.classList.add("disabled");
+  } else {
+    elPreBtn.diabled = false;
+    // elPreBtn.classList.remove("disabled");
+  }
+
+  const lastPage = Math.ceil(data.totalResults / 10);
+
+  if (APIPage === lastPage) {
+    elNextBtn.disabled = true;
+    // elNextBtn.classList.add("disabled");
+  } else {
+    elNextBtn.disabled = false;
+    // elNextBtn.classList.add("disabled");
+  }
+}
+
+// pagination function
+elNextBtn.addEventListener("click", (evt) => {
+  console.log("plus");
+  APIPage++;
+  fetchFilms();
+});
+
+elPreBtn.addEventListener("click", (evt) => {
+  console.log("minus");
+  APIPage--;
+  fetchFilms();
+});
+
 // form submit
 elForm.addEventListener("submit", (evt) => {
+  APISearch = elMoviesInput.value.trim();
   evt.preventDefault();
-
-  async function fetchFilms() {
-    const APIKey = "2b278381";
-    const APISearch = elMoviesInput.value;
-    let APIPage = 1;
-    const response = await fetch(
-      "http://www.omdbapi.com/?apikey=" +
-        APIKey +
-        "&s=" +
-        APISearch +
-        "&page=" +
-        APIPage
-    );
-
-    const data = await response.json();
-    const filmArr = data.Search;
-
-    console.log(data);
-    const moviesInput = elMoviesInput.value.trim();
-    const regex = new RegExp(moviesInput, "gi");
-    const searchedFilms = filmArr.filter((film) => film.Title.match(regex));
-
-    filmRender(searchedFilms, elMovieList);
-  }
 
   fetchFilms();
 });
 
-// // Rendering films
+// Rendering films
 function filmRender(filmArr, element) {
   element.innerHTML = null;
+
+  const elFragment = document.createDocumentFragment();
+
   filmArr.forEach((film) => {
     const movieTemplate = elMovieTemplate.cloneNode(true);
 
@@ -89,7 +127,9 @@ function filmRender(filmArr, element) {
       elModalType.textContent = "Type: " + foundFilms.Type.toUpperCase();
       elModalImdb.textContent = "IMDB: " + foundFilms.imdbID;
     });
-
-    element.appendChild(movieTemplate);
+    elFragment.appendChild(movieTemplate);
   });
+  element.appendChild(elFragment);
 }
+
+fetchFilms();
